2edefc12cbc4fcb09c59bed121688c649368a79c
Zsolt <5047426+bodzaital@users.noreply.github.com>
2026-01-25 22:45:58 +0100

    Reorganize Collections.cs

--- AllaDb/Collection.cs
+++ AllaDb/Collection.cs
@@ -11,7 +11,6 @@ public class Collection(AllaOptions options, string name)
 	private readonly AllaOptions _options = options;
 
 	/// <summary>Gets the unique name of this <see cref="Collection"/>.</summary>
-	[JsonInclude]
 	public string Name { get; set; } = name;
 
 	[JsonInclude]
@@ -30,23 +29,7 @@ public class Collection(AllaOptions options, string name)
 		SetCollectionToConstraints();
 	}
 
-	/// <summary>Assigns a range of <see cref="IConstraint"/>s to the <see cref="Collection"/>.</summary>
-	/// <param name="constraints">The range of <see cref="IConstraint"/>s whose elements should be assigned to the <see cref="Collection"/>.</param>
-	/// <returns>The created constraints.</returns>
-	public List<IConstraint> CreateConstraints(params IEnumerable<IConstraint> constraints)
-	{
-		Constraints.AddRange(constraints);
-		SetCollectionToConstraints();
-		return Constraints;
-	}
-
-	public void DeleteConstraint(string constraintId)
-	{
-		IConstraint constraint = Constraints.FirstOrDefault((x) => x.Id == constraintId)
-			?? throw new ArgumentOutOfRangeException(nameof(constraintId), constraintId);
-
-		Constraints.Remove(constraint);
-	}
+	#region Collections
 
 	/// <summary>Removes all <see cref="Document"/>s from the <see cref="Collection"/>.</summary>
 	public void Truncate()
@@ -57,18 +40,36 @@ public class Collection(AllaOptions options, string name)
 		else Documents.Clear();
 	}
 
-	/// <summary>Deletes the <see cref="Document"/> with the specified Id from the <see cref="Collection"/>. If the specified <see cref="Document"/> is not found, throws an <see cref="ArgumentOutOfRangeException"/>.</summary>
-	/// <param name="documentId">The Id of the <see cref="Document"/> to delete.</param>
-	public void DeleteDocument(string documentId)
+	#endregion
+
+	#region Constraints
+
+	/// <summary>Assigns a range of <see cref="IConstraint"/>s to the <see cref="Collection"/>.</summary>
+	/// <param name="constraints">The range of <see cref="IConstraint"/>s whose elements should be assigned to the <see cref="Collection"/>.</param>
+	/// <returns>The created constraints.</returns>
+	public void CreateConstraints(params IEnumerable<IConstraint> constraints)
 	{
-		ThrowIfRequiredTransactionMissing();
-
-		Document document = GetDocuments().FirstOrDefault((x) => x.Id == documentId)
-			?? throw new ArgumentOutOfRangeException(nameof(documentId), documentId);
-
-		if (OpenTransaction is not null) OpenTransaction.AddDocumentDeletion(document);
-		else Documents.Remove(document);
+		Constraints.AddRange(constraints);
+		SetCollectionToConstraints();
 	}
+
+	/// <summary>Deletes the <see cref="IConstraint"/> with the specified Id from the <see cref="Collection"/>. If the specified <see cref="IConstraint"/> is not found, throws an <see cref="ArgumentOutOfRangeException"/>.</summary>
+	/// <param name="constraintId">The Id of the <see cref="IConstraint"/> to delete.</param>
+	public void DeleteConstraint(string constraintId)
+	{
+		IConstraint constraint = Constraints.FirstOrDefault((x) => x.Id == constraintId)
+			?? throw new ArgumentOutOfRangeException(nameof(constraintId), constraintId);
+
+		Constraints.Remove(constraint);
+	}
+
+	/// <summary>Returns a <see cref="ReadOnlyCollection{IConstraint}"/> of the <see cref="IConstraint"/>s of the <see cref="Collection"/>.</summary>
+	/// <returns>A <see cref="ReadOnlyCollection{IConstraint}"/> that can be used to iterate over the <see cref="IConstraint"/>s of the <see cref="Collection"/>.</returns>
+	public ReadOnlyCollection<IConstraint> GetConstraints() => new(Constraints);
+
+	#endregion
+
+	#region Documents
 
 	/// <summary>Adds a <see cref="Document"/> to the <see cref="Collection"/>.</summary>
 	/// <param name="fields">The <see cref="Dictionary{string, object?}"/> whose elements are copied to the new <see cref="Document"/>.</param>
@@ -86,23 +87,24 @@ public class Collection(AllaOptions options, string name)
 		return document;
 	}
 
+	/// <summary>Deletes the <see cref="Document"/> with the specified Id from the <see cref="Collection"/>. If the specified <see cref="Document"/> is not found, throws an <see cref="ArgumentOutOfRangeException"/>.</summary>
+	/// <param name="documentId">The Id of the <see cref="Document"/> to delete.</param>
+	public void DeleteDocument(string documentId)
+	{
+		ThrowIfRequiredTransactionMissing();
+
+		Document document = GetDocuments().FirstOrDefault((x) => x.Id == documentId)
+			?? throw new ArgumentOutOfRangeException(nameof(documentId), documentId);
+
+		if (OpenTransaction is not null) OpenTransaction.AddDocumentDeletion(document);
+		else Documents.Remove(document);
+	}
+
 	/// <summary>Gets the <see cref="Document"/> associated with the specified Id.</summary>
 	/// <param name="documentId">The Id of the <see cref="Document"/> to get.</param>
 	/// <returns>The <see cref="Document"/> associated with the specified Id. If the specified Id is not found, throws an <see cref="ArgumentOutOfRangeException"/>.</returns>
 	public Document GetDocument(string documentId) => GetDocuments().FirstOrDefault((x) => x.Id == documentId)
 		?? throw new ArgumentOutOfRangeException(nameof(documentId), documentId);
-
-	/// <summary>Creates a <see cref="Transaction"/> on the <see cref="Collection"/>.</summary>
-	/// <returns>The created <see cref="Transaction"/>. If the <see cref="Collection"/> already has a <see cref="Transaction"/> in-progress, throws <see cref="UnresolvedTransactionException"/>.</returns>
-	public Transaction CreateTransaction()
-	{
-		if (OpenTransaction is not null) throw new UnresolvedTransactionException(
-			"Resolve the transaction before opening a new transaction."
-		);
-		
-		OpenTransaction = new(this);
-		return OpenTransaction;
-	}
 
 	/// <summary>Returns a <see cref="ReadOnlyCollection{Document}"/> of the <see cref="Document"/>s of the <see cref="Collection"/>.</summary>
 	/// <returns>A <see cref="ReadOnlyCollection{Document}"/> that can be used to iterate over the <see cref="Document"/>s of the <see cref="Collection"/>.</returns>
@@ -117,6 +119,24 @@ public class Collection(AllaOptions options, string name)
 
 		return new([..notDeletedDocuments, ..OpenTransaction.Documents(Transaction.ChangeAction.Written)]);
 	}
+
+	#endregion
+
+	#region Transactions
+
+	/// <summary>Creates a <see cref="Transaction"/> on the <see cref="Collection"/>.</summary>
+	/// <returns>The created <see cref="Transaction"/>. If the <see cref="Collection"/> already has a <see cref="Transaction"/> in-progress, throws <see cref="UnresolvedTransactionException"/>.</returns>
+	public Transaction CreateTransaction()
+	{
+		if (OpenTransaction is not null) throw new UnresolvedTransactionException(
+			"Resolve the transaction before opening a new transaction."
+		);
+		
+		OpenTransaction = new(this);
+		return OpenTransaction;
+	}
+
+	#endregion
 
 	internal void ThrowIfRequiredTransactionMissing()
 	{
