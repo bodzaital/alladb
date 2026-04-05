4ae28faca82119a51889c653ffb10335ab5e130c
Zsolt <5047426+bodzaital@users.noreply.github.com>
2026-04-05 16:28:39 +0200

    Add document-transaction tests

---
+++ AllaDb.Tests/DocumentKeyDeleteTests.cs
@@ -0,0 +1,170 @@
+﻿namespace AllaDb.Tests;
+
+public class DocumentKeyDeleteTests
+{
+    [Test]
+    public void CanDeleteKey_WithNoTransaction()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        document.Remove("key");
+        bool resultExists = document.TryGetValue("key", out string? resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+    }
+
+    [Test]
+    public void CanDeleteKey_WithOneTransaction_WithRollback()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction = collection.CreateTransaction();
+        document.Remove("key");
+        bool resultExists = document.TryGetValue("key", out string? resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+
+		transaction.Rollback().Dispose();
+
+        resultExists = document.TryGetValue("key", out resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(resultExists, Is.True);
+			Assert.That(resultValue, Is.EqualTo("original value"));
+		}
+	}
+
+    [Test]
+    public void CanDeleteKey_WithOneTransaction_WithDefaultCommit()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction = collection.CreateTransaction();
+        document.Remove("key");
+        bool resultExists = document.TryGetValue("key", out string? resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+
+		transaction.Dispose();
+
+        resultExists = document.TryGetValue("key", out resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+	}
+
+    [Test]
+    public void CanDeleteKey_WithOneTransaction_WithExplicitCommit()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction = collection.CreateTransaction();
+        document.Remove("key");
+        bool resultExists = document.TryGetValue("key", out string? resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+
+		transaction.Commit().Dispose();
+
+        resultExists = document.TryGetValue("key", out resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+	}
+
+    [Test]
+    public void CanDeleteKey_WithTwoTransactionsMiddleReadd_WithRollback()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction1 = collection.CreateTransaction();
+
+        document.Remove("key");
+        bool resultExists = document.TryGetValue("key", out string? resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+
+		Transaction transaction2 = collection.CreateTransaction();
+
+        document.AddOrUpdate("key", "middle readd");
+        resultExists = document.TryGetValue("key", out resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(resultExists, Is.True);
+			Assert.That(resultValue, Is.EqualTo("middle readd"));
+		}
+
+		transaction2.Rollback().Dispose();
+
+        resultExists = document.TryGetValue("key", out resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(resultExists, Is.False);
+			Assert.That(resultValue, Is.Null);
+		}
+
+		transaction1.Rollback().Dispose();
+
+        resultExists = document.TryGetValue("key", out resultValue);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(resultExists, Is.True);
+			Assert.That(resultValue, Is.EqualTo("original value"));
+		}
+	}
+
+    private static Collection CreateTestCollection() => new()
+    {
+        Name = "Test collection"
+    };
+
+    private static Dictionary<string, object?> CreateTestField() => new()
+    {
+        { "key", "original value" },
+    };
+}
---
+++ AllaDb.Tests/DocumentKeyUpdateTests.cs
@@ -0,0 +1,160 @@
+﻿namespace AllaDb.Tests;
+
+public class DocumentKeyUpdateTests
+{
+    [Test]
+    public void CanUpdateKey_WithNoTransaction()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        document.AddOrUpdate("key", "updated value");
+        document.TryGetValue("key", out string? result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(result, Is.EqualTo("updated value"));
+		}
+    }
+
+    [Test]
+    public void CanUpdateKey_WithOneTransaction_WithRollback()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction = collection.CreateTransaction();
+        document.AddOrUpdate("key", "updated value");
+        document.TryGetValue("key", out string? result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(result, Is.EqualTo("updated value"));
+		}
+
+		transaction.Rollback().Dispose();
+
+        document.TryGetValue("key", out result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(result, Is.EqualTo("original value"));
+		}
+	}
+
+    [Test]
+    public void CanUpdateKey_WithOneTransaction_WithDefaultCommit()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction = collection.CreateTransaction();
+        document.AddOrUpdate("key", "updated value");
+        document.TryGetValue("key", out string? result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(result, Is.EqualTo("updated value"));
+		}
+
+		transaction.Dispose();
+
+        document.TryGetValue("key", out result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(result, Is.EqualTo("updated value"));
+		}
+	}
+
+    [Test]
+    public void CanUpdateKey_WithOneTransaction_WithExplicitCommit()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction = collection.CreateTransaction();
+        document.AddOrUpdate("key", "updated value");
+        document.TryGetValue("key", out string? result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(result, Is.EqualTo("updated value"));
+		}
+
+		transaction.Commit().Dispose();
+
+        document.TryGetValue("key", out result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(result, Is.EqualTo("updated value"));
+		}
+	}
+
+    [Test]
+    public void CanUpdateKey_WithTwoTransactions_WithRollback()
+    {
+        Collection collection = CreateTestCollection();
+        Document document = collection.Add(CreateTestField());
+
+        Transaction transaction1 = collection.CreateTransaction();
+
+        document.AddOrUpdate("key", "first update");
+        document.TryGetValue("key", out string? result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(result, Is.EqualTo("first update"));
+		}
+
+		Transaction transaction2 = collection.CreateTransaction();
+
+        document.AddOrUpdate("key", "second update");
+        document.TryGetValue("key", out result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(result, Is.EqualTo("second update"));
+		}
+
+		transaction2.Rollback().Dispose();
+
+        document.TryGetValue("key", out result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.True);
+			Assert.That(result, Is.EqualTo("first update"));
+		}
+
+		transaction1.Rollback().Dispose();
+
+        document.TryGetValue("key", out result);
+
+		using (Assert.EnterMultipleScope())
+		{
+			Assert.That(collection.HasTransaction, Is.False);
+			Assert.That(result, Is.EqualTo("original value"));
+		}
+	}
+
+    private static Collection CreateTestCollection() => new()
+    {
+        Name = "Test collection"
+    };
+
+    private static Dictionary<string, object?> CreateTestField() => new()
+    {
+        { "key", "original value" },
+    };
+}
